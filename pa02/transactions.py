bc22fda78b3befc04d85eec841da8afd518c0853
Connor Lima <connorlima@connors-air.dyn.brandeis.edu>
2022-03-24 20:52:23 -0400

    added delete and summarize_by_month methods to Transaction (parts 6 and 8)

--- pa02/transactions.py
+++ pa02/transactions.py
@@ -11,3 +11,20 @@ class Transaction:
                     description text)")
         con.commit()
         con.close()
+
+    def delete(self, itemnumber):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+        cur.execute("DELETE FROM transactions WHERE itemnumber=(?);",
+                    (itemnumber,))
+        con.commit()
+        con.close()
+
+    def summarize_by_month(self, month):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM transactions WHERE date%10000/100=(?);",
+                    (month,))
+        transactions = cur.fetchall()
+        con.close()
+        return self.list_to_dict(transactions)
